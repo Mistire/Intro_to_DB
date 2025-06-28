0462b0dd02bf1a4a23aeb8cf70c4267fa9bf89aa
Mistire <mdiwithgod@gmail.com>
2025-06-29 00:46:57 +0300

    Write a script that inserts a multiple row in the table

---
+++ task_6.sql
@@ -0,0 +1,3 @@
+INSERT INTO customer (
+  customer_id, customer_name, email, address
+) VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.'), VALUES (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'), VALUES (3, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
